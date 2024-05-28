f3ed17de5b570ecdcc843a8acfcc8b816183c597
hakimamarullah <hakimamarullah@gmail.com>
2024-05-28 18:30:53 +0700

    Initial commit

---
+++ src/main/java/com/vascomm/vascomm/springboot/VascommSpringbootApplication.java
@@ -0,0 +1,13 @@
+package com.vascomm.vascomm.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VascommSpringbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VascommSpringbootApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/vascomm/vascomm/springboot/VascommSpringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vascomm.vascomm.springboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VascommSpringbootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
